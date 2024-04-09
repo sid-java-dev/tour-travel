fcf3b788af88f4f7ce24039ce7220709c04ab4f1
Admin-github123 <patelsiddhant9297@gmail.com>
2024-04-09 13:02:02 +0530

    second commit

---
+++ src/main/java/com/airbnb/TourTravelsApplication.java
@@ -0,0 +1,13 @@
+package com.airbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourTravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourTravelsApplication.class, args);
+	}
+
+}
